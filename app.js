4db97b353cbdf70674da4ab0cf54995360a585d7
rajatjhamnani <rajatjhamnani@gmail.com>
2024-09-13 18:17:14 +0530

    Intro to Middlewares(with some modifications)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app=express()
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/add-product',(req , res , next)=>{
   console.log('i am middleware')
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>')
+  res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="number" name="size" min="0"><button type="submit">ADD PRODUCT</button></form>')
 })
 app.post('/product', (req , res , next)=>{
   console.log(req.body)
